2c5f1e904398cb71bcda47a28a1955ab3c72a81d
meenu10802 <meenu10802@gmail.com>
2026-03-26 17:14:14 +0530

    [Meenakshi]:UC2: Added query parameter API

--- src/main/java/org/example/helloapp/HelloController.java
+++ src/main/java/org/example/helloapp/HelloController.java
@@ -8,8 +8,13 @@ import org.springframework.web.bind.annotation.*;
 public class HelloController {
 
     // UC1
-    @GetMapping("")
+
     public String sayHello() {
         return "Hello from BridgeLabz";
+    }
+    // UC2
+    @GetMapping("/query")
+    public String sayHelloQuery(@RequestParam String name) {
+        return "Hello " + name + " from BridgeLabz";
     }
 }
