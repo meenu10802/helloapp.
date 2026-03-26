1745b02009561e1dcba60986572eb28e2b8918cf
meenu10802 <meenu10802@gmail.com>
2026-03-26 17:03:38 +0530

    [Meenakshi]: Added project structure

---
+++ src/main/java/org/example/helloapp/HelloappApplication.java
@@ -0,0 +1,12 @@
+package org.example.helloapp;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloappApplication.class, args);
+    }
+
+}
